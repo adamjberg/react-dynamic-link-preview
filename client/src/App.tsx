3da8e5f9883de574146828bf5a714f1265593fca
Adam Berg <adam@xyzdigital.com>
2021-11-27 11:33:27 -0800

    Add React Helmet

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { Helmet } from "react-helmet";
 
 export const App: React.FC = () => {
     const [clientMessage, setClientMessage] = useState("");
@@ -7,8 +8,13 @@ export const App: React.FC = () => {
         setClientMessage("Hello from the client!")
     }, [setClientMessage]);
 
-    return <>
-        <h1>Hello from the server!</h1>
-        <h2>{clientMessage}</h2>
-    </>
+    return (
+        <div className="app">
+            <Helmet>
+                <title>React Helmet Title</title>
+            </Helmet>
+            <h1>Hello from the server!</h1>
+            <h2>{clientMessage}</h2>
+        </div>
+    )
 }
